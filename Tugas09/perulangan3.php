a2e10c639f0ff5579ddbd7097ec3fc63c43ad75e
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2024-11-27 21:58:38 +0700

    perbaikan percabangan

--- Tugas09/perulangan3.php
+++ Tugas09/perulangan3.php
@@ -1,27 +1,27 @@
 <?php
+do {
+    $angka = readline("Silahkan masukan angka: ");
+    $totalDigit = 0;
 
-$angka = readline("Silahkan masukan angka: ");
-$totalDigit = 0;
-
-// perulangan untuk menghitung input dari pengguna
-for ($i = 0; $i < strlen($angka); $i++) {
-    if (ctype_digit($angka[$i])) {
-        $totalDigit += (int)$angka[$i];
+    // perulangan untuk menghitung input dari pengguna
+    for ($i = 0; $i < strlen($angka); $i++) {
+        if (ctype_digit($angka[$i])) {
+            $totalDigit += (int)$angka[$i];
+        }
     }
-}
 
-echo"-------------------------------------\n";
-echo "Jumlah total digit = " . $totalDigit . "\n";
+    echo"-------------------------------------\n";
+    echo "Jumlah total digit = " . $totalDigit . "\n";
 
-// validasi input, jika pengguna salah memasukan yang bukan angka
-if (!ctype_digit($angka)) {
-    echo "Peringatan: Input tidak sepenuhnya angka.\n";
-}
-
-// fitur untuk mengulangi input
-$ulangi = readline("Apakah ingin mengulangi? (y/t): ");
-if (strtolower($ulangi) == 'y') {
-    // panggil script ini lagi
-    include __FILE__;
+    // validasi input, jika pengguna salah memasukan yang bukan angka
+    if (!ctype_digit($angka)) {
+        echo "Peringatan: Input tidak sepenuhnya angka.\n";
     }
+
+    // fitur untuk mengulangi input
+    $ulangi = readline("Apakah ingin mengulangi? (y/t): ");
+}   while (strtolower($ulangi) == 'y');
+
+echo "Program selesai. Terima kasih!\n";
+echo"---------------------------------------\n";
 ?>
